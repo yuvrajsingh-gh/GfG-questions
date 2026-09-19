11554de85ba2c2aef4df2fb3e840ebff219674a9
Yuvraj Singh <yuvrajsingh.contact@gmail.com>
2026-09-19 15:13:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pyramid Pattern/pyramid-pattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
