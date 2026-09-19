183ef74e7a0d1f2c521f0b57e2d6394738beaa42
Yuvraj Singh <yuvrajsingh.contact@gmail.com>
2026-09-19 14:08:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Solid Square/print-solid-square.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
