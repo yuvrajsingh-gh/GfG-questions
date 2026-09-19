32c8891c0772cac5403b645be477d7eaa07f83ac
Yuvraj Singh <yuvrajsingh.contact@gmail.com>
2026-09-19 14:18:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Solid Right Angle Triangle Pattern/solid-right-angle-triangle-pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
